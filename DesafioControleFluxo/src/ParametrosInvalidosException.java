e9b30f34aa26a4073b25da18ab0588971cda28b3
wesley <wesleyy.dev2@gmail.com>
2025-04-27 00:36:01 -0300

    Implementando Desafio Controle de Fluxo

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,5 +1,3 @@
 public class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException() {
-        super("O segundo parâmetro deve ser maior que o primeiro.");
-    }
+
 }
